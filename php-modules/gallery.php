c93e0987ae58657851744f652a6054f9ae006395
ipbyrne <ipbyrne@IsaacParnesiMac.PK5001Z>
2015-11-04 00:50:21 -0700

    finished gallery

--- php-modules/gallery.php
+++ php-modules/gallery.php
@@ -116,21 +116,25 @@
     </div>
   
   <script type="text/javascript">
-    //array of sidplayed images -- Always contains 12
+    // Need array of sidplayed images -- Always contains 12
     var displayedImages  = ["http://www.tisaneteas.com/images/demo1.jpg","http://www.tisaneteas.com/images/demo2.jpg","http://www.tisaneteas.com/images/demo3.jpg","http://www.tisaneteas.com/images/demo4.jpg","http://www.tisaneteas.com/images/demo5.jpg","http://www.tisaneteas.com/images/demo6.jpg","http://www.tisaneteas.com/images/demo7.jpg","http://www.tisaneteas.com/images/demo8.jpg","http://www.tisaneteas.com/images/demo9.jpg","http://www.tisaneteas.com/images/demo10.jpg","http://www.tisaneteas.com/images/demo11.jpg","http://www.tisaneteas.com/images/demo12.jpg"];
     
-    // aray of undisplayed images
+    // aray of undisplayed images 
     // To add more images simply add a comma to the end of the array followed by the image url in quotations
     var undisplayedImage = ["http://www.tisaneteas.com/images/demo13.jpg","http://www.tisaneteas.com/images/demo14.jpg","http://www.tisaneteas.com/images/demo15.jpg","http://www.tisaneteas.com/images/demo16.jpg","http://www.tisaneteas.com/images/demo17.jpg","http://www.tisaneteas.com/images/demo18.jpg","http://www.tisaneteas.com/images/demo19.jpg"];
     // Need to switch items in array, and replace current src
     // Rotate Image Based on Time
-    var rotate_them = setInterval(function(){rotating()},2000); // Last input is frequency of switch in milliseconds.
+    var rotate_them = setInterval(function(){rotating()},1500); // Last input is frequency of switch image changes in milliseconds.
     
     
     function rotating() {
       // Find with image to switch out
       // Use math.floor on a random number between 1 &  12, grab "url"+number+'.jpg'
         var firstNumber = Math.floor(Math.random() * ((displayedImages.length -1) - 0)) + 0;
+      
+       // Find with image to switch in
+      // Use math.floor on a random number between 1 &  7, grab "url"+number+'.jpg'
+        var secondNumber = Math.floor(Math.random() * ((undisplayedImage.length -1) - 0)) + 0
       // Set up each card to be flipped
       for(var i = 0; i <= 11; i++) {
           $("#card"+ (i + 1)).flip({
@@ -143,28 +147,27 @@
       // Flip the Card
       $("#card"+(firstNumber+1)).flip('toggle');
       
-      // Find with image to switch in
-      // Use math.floor on a random number between 1 &  7, grab "url"+number+'.jpg'
-        var secondNumber = Math.floor(Math.random() * ((undisplayedImage.length -1) - 0)) + 0
+     
         
         var imageToBeSwapped = displayedImages[firstNumber];
         var newImage = undisplayedImage[secondNumber];
-        
         displayedImages[firstNumber] = newImage;
         undisplayedImage[secondNumber] = imageToBeSwapped;
-        
-        $(".image1").attr("src", displayedImages[0]);
-        $(".image2").attr("src", displayedImages[1]);
-        $(".image3").attr("src", displayedImages[2]);
-        $(".image4").attr("src", displayedImages[3]);
-        $(".image5").attr("src", displayedImages[4]);
-        $(".image6").attr("src", displayedImages[5]);
-        $(".image7").attr("src", displayedImages[6]);
-        $(".image8").attr("src", displayedImages[7]);
-        $(".image9").attr("src", displayedImages[8]);
-        $(".image10").attr("src", displayedImages[9]);
-        $(".image11").attr("src", displayedImages[10]);
-        $(".image12").attr("src", displayedImages[11]);                                   
+        setTimeout(function() {
+          $(".image1").attr("src", displayedImages[0]);
+          $(".image2").attr("src", displayedImages[1]);
+          $(".image3").attr("src", displayedImages[2]);
+          $(".image4").attr("src", displayedImages[3]);
+          $(".image5").attr("src", displayedImages[4]);
+          $(".image6").attr("src", displayedImages[5]);
+          $(".image7").attr("src", displayedImages[6]);
+          $(".image8").attr("src", displayedImages[7]);
+          $(".image9").attr("src", displayedImages[8]);
+          $(".image10").attr("src", displayedImages[9]);
+          $(".image11").attr("src", displayedImages[10]);
+          $(".image12").attr("src", displayedImages[11]);    
+        }, 250) //Last input is time which should be 1/4 of the speed of the card flip                                
     }
+    
     
   </script>
