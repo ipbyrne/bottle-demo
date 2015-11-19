58fe35c21f28e2cf62ca024317bd6e64a548a01c
ipbyrne <king.sisyphus94@gmail.com>
2015-11-19 13:03:54 -0700

    Updated gallery.php

--- php-modules/gallery.php
+++ php-modules/gallery.php
@@ -1,118 +1,16 @@
  <div id="demo-container">
-        <div id="card1"> 
-          <div class="front"> 
-            <img class="image1" src="http://www.tisaneteas.com/images/demo1.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image1" src="http://www.tisaneteas.com/images/demo1.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card2" style="left: 25%;"> 
-          <div class="front"> 
-            <img class="image2" src="http://www.tisaneteas.com/images/demo2.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image2" src="http://www.tisaneteas.com/images/demo2.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card3" style="left: 50%;"> 
-          <div class="front"> 
-            <img class="image3" src="http://www.tisaneteas.com/images/demo3.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image3" src="http://www.tisaneteas.com/images/demo3.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card4" style="left: 75%;"> 
-          <div class="front"> 
-            <img class="image4" src="http://www.tisaneteas.com/images/demo4.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image4" src="http://www.tisaneteas.com/images/demo4.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-    
-        <div style="height:163px"></div>
-        <div id="card5"> 
-          <div class="front"> 
-            <img class="image5" src="http://www.tisaneteas.com/images/demo5.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image5" src="http://www.tisaneteas.com/images/demo5.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card6" style="left: 25%;"> 
-          <div class="front"> 
-            <img class="image6" src="http://www.tisaneteas.com/images/demo6.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image6" src="http://www.tisaneteas.com/images/demo6.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card7" style="left: 50%;"> 
-          <div class="front"> 
-            <img class="image7" src="http://www.tisaneteas.com/images/demo7.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image7" src="http://www.tisaneteas.com/images/demo7.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card8" style="left: 75%;"> 
-          <div class="front"> 
-            <img class="image8" src="http://www.tisaneteas.com/images/demo8.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image8" src="http://www.tisaneteas.com/images/demo8.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-    
-        <div style="height:163px"></div>
-        <div id="card9"> 
-          <div class="front"> 
-            <img class="image9" src="http://www.tisaneteas.com/images/demo9.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image9" src="http://www.tisaneteas.com/images/demo9.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card10" style="left: 25%;"> 
-          <div class="front"> 
-            <img class="image10" src="http://www.tisaneteas.com/images/demo10.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image10" src="http://www.tisaneteas.com/images/demo10.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card11" style="left: 50%;"> 
-          <div class="front"> 
-            <img class="image11" src="http://www.tisaneteas.com/images/demo11.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image11" src="http://www.tisaneteas.com/images/demo11.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-        <div id="card12" style="left: 75%;"> 
-          <div class="front"> 
-            <img class="image12" src="http://www.tisaneteas.com/images/demo12.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-          <div class="back">
-            <img class="image12" src="http://www.tisaneteas.com/images/demo12.jpg" width="24.5%" nopin="nopin"/>
-          </div> 
-        </div>
-    
-    
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
-        <img class="image" src="http://www.transparenttextures.com/patterns/asfalt-light.png" width="24.5%" nopin="nopin"/>
+      <img class="image1" src="http://www.tisaneteas.com/images/demo1.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image2" src="http://www.tisaneteas.com/images/demo2.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image3" src="http://www.tisaneteas.com/images/demo3.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image4" src="http://www.tisaneteas.com/images/demo4.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image5" src="http://www.tisaneteas.com/images/demo5.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image6" src="http://www.tisaneteas.com/images/demo6.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image7" src="http://www.tisaneteas.com/images/demo7.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image8" src="http://www.tisaneteas.com/images/demo8.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image9" src="http://www.tisaneteas.com/images/demo9.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image10" src="http://www.tisaneteas.com/images/demo10.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image11" src="http://www.tisaneteas.com/images/demo11.jpg" width="24.5%" nopin="nopin"/>
+      <img class="image12" src="http://www.tisaneteas.com/images/demo12.jpg" width="24.5%" nopin="nopin"/> 
     </div>
   
   <script type="text/javascript">
@@ -136,38 +34,18 @@
       // Use math.floor on a random number between 1 &  7, grab "url"+number+'.jpg'
         var secondNumber = Math.floor(Math.random() * ((undisplayedImage.length -1) - 0)) + 0
       // Set up each card to be flipped
-      for(var i = 0; i <= 11; i++) {
-          $("#card"+ (i + 1)).flip({
-            axis: 'x',
-            speed: '1000', // speed is how quickly the flip animation occurs.
-            trigger: 'manual'
-          });
-      }
-
-      // Flip the Card
-      $("#card"+(firstNumber+1)).flip('toggle');
-      
-     
-        
         var imageToBeSwapped = displayedImages[firstNumber];
         var newImage = undisplayedImage[secondNumber];
-        displayedImages[firstNumber] = newImage;
+        displayedImages[firstNumber] = newImage; 
         undisplayedImage[secondNumber] = imageToBeSwapped;
-        setTimeout(function() {
-          $(".image1").attr("src", displayedImages[0]);
-          $(".image2").attr("src", displayedImages[1]);
-          $(".image3").attr("src", displayedImages[2]);
-          $(".image4").attr("src", displayedImages[3]);
-          $(".image5").attr("src", displayedImages[4]);
-          $(".image6").attr("src", displayedImages[5]);
-          $(".image7").attr("src", displayedImages[6]);
-          $(".image8").attr("src", displayedImages[7]);
-          $(".image9").attr("src", displayedImages[8]);
-          $(".image10").attr("src", displayedImages[9]);
-          $(".image11").attr("src", displayedImages[10]);
-          $(".image12").attr("src", displayedImages[11]);    
-        }, 250) //Last input is time which should be 1/4 of the speed of the card flip                                
+      
+         
+        $(".image"+(firstNumber+1)).fadeOut(
+          function(){
+            $(".image"+(firstNumber+1)).attr('src', displayedImages[firstNumber]);
+            $(".image"+(firstNumber+1)).fadeIn();
+          }
+        ); // end fadeOut                    
     }
-    
     
   </script>
