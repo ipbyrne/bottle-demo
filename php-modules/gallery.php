122d1680215cd0371ff1a9f7282d366704765892
ipbyrne <king.sisyphus94@gmail.com>
2015-11-19 13:53:55 -0700

    Update gallery.php

--- php-modules/gallery.php
+++ php-modules/gallery.php
@@ -1,4 +1,7 @@
- <div id="demo-container">
+ <div id="demo-container" style="
+    height: inherit;
+    background: none;
+">
       <img class="image1" src="http://www.tisaneteas.com/images/demo1.jpg" width="24.5%" nopin="nopin"/>
       <img class="image2" src="http://www.tisaneteas.com/images/demo2.jpg" width="24.5%" nopin="nopin"/>
       <img class="image3" src="http://www.tisaneteas.com/images/demo3.jpg" width="24.5%" nopin="nopin"/>
